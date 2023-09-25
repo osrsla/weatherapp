0a35c74fcc39971ce50713fe8211af6c8c61bb73
OSR <osrsla@gmail.com>
2023-09-25 12:49:59 +0900

    Initial Commit

---
+++ WeatherApp-nbc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp-nbc
+//
+//  Created by SR on 2023/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
